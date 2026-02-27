fed1dc2a5c951849f7a8f606274c60cafef1a702
Elliot Datsomor <datsomorelliot56@gmail.com>
2026-02-27 23:43:00 +0000

    change the controller

--- DATLEP/apps/order-service/src/controllers/order.controller.ts
+++ DATLEP/apps/order-service/src/controllers/order.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import {  Request, Response } from "express";
 // import redis from "packages/libs/redis";
 
 export const testOrderController = (req: Request, res: Response) => {
